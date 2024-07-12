abbc9799ff885ddb8f0e2019ac08bc896bff8a64
Mark Wieringa <Mark.Wieringa@csiro.au>
2024-07-12 13:35:26 +1000

    AXA-3053 remove old or unused code and refactor
    AXA-3053 review mods

--- askap/deconvolution/DeconvolverMultiTermBasisFunction.h
+++ askap/deconvolution/DeconvolverMultiTermBasisFunction.h
@@ -10,7 +10,7 @@
 /// @ingroup Deconvolver
 ///
 ///
-/// @copyright (c) 2007 CSIRO
+/// @copyright (c) 2007,2024 CSIRO
 /// Australia Telescope National Facility (ATNF)
 /// Commonwealth Scientific and Industrial Research Organisation (CSIRO)
 /// PO Box 76, Epping NSW 1710, Australia
@@ -33,6 +33,7 @@
 /// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 ///
 /// @author Tim Cornwell <tim.cornwell@csiro.au>
+/// @author Mark Wieringa <Mark.Wieringa@csiro.au>
 ///
 
 #ifndef ASKAP_SYNTHESIS_DECONVOLVERMULTITERMBASISFUNCTION_H
@@ -40,50 +41,11 @@
 
 #include <string>
 
-#include <casacore/casa/aips.h>
 #include <boost/shared_ptr.hpp>
-#include <casacore/casa/Arrays/Array.h>
 
 #include <askap/deconvolution/DeconvolverBase.h>
-#include <askap/deconvolution/DeconvolverState.h>
-#include <askap/deconvolution/DeconvolverControl.h>
-#include <askap/deconvolution/DeconvolverMonitor.h>
 #include <askap/deconvolution/BasisFunction.h>
 
-
-#ifdef USE_OPENACC
-template<class T>
-class ACCManager {
-
-    public:
-
-        ACCManager();
-        ~ACCManager();
-
-        void CopyToDevice();
-        void InitMask(int base);
-        void UpdateMask(int base);
-
-        size_t nBases;
-        size_t nTerms;
-        size_t npixels;
-        size_t nrows;
-        size_t ncols;
-
-        Matrix<T> tmpMask;
-
-        uInt64 *residuals;
-        uInt64 *masks;
-        T** coefficients;
-
-        T* maskToUse;
-        T* weight;
-
-        Bool * deleteResiduals;
-        Bool * deleteMasks;
-        Bool * deleteCoefficients;
-};
-#endif // USE_OPENACC
 
 namespace askap {
 
@@ -96,15 +58,14 @@ namespace askap {
         /// to improve performance and flexibility.
         ///
         /// The template argument T is the type, and FT is the transform
-        /// e.g. DeconvolverBasisFunction<Double, DComplex>
+        /// e.g. DeconvolverBasisFunction<double, DComplex>
         /// @ingroup Deconvolver
         template<class T, class FT>
         class DeconvolverMultiTermBasisFunction : public DeconvolverBase<T, FT> {
 
             public:
 
-                typedef boost::shared_ptr<DeconvolverMultiTermBasisFunction<T, FT> > ShPtr;
-
+                typedef boost::shared_ptr<DeconvolverMultiTermBasisFunction<T, FT>> ShPtr;
 
                 /// @brief Construct from dirty image and psf
                 /// @detail Construct a deconvolver from a dirty image and
@@ -115,9 +76,9 @@ namespace askap {
                 /// @param[in] dirty Dirty image (array)
                 /// @param[in] psf Point Spread Function (array)
                 /// @param[in] psf Point Spread Function containing 2*nTaylor-1 terms (array)
-                DeconvolverMultiTermBasisFunction(casacore::Vector<casacore::Array<T> >& dirty,
-                                                  casacore::Vector<casacore::Array<T> >& psf,
-                                                  casacore::Vector<casacore::Array<T> >& psfLong);
+                DeconvolverMultiTermBasisFunction(Vector<Array<T>>& dirty,
+                                                  Vector<Array<T>>& psf,
+                                                  Vector<Array<T>>& psfLong);
 
                 /// @brief Construct from dirty image and psf
                 /// @detail Construct a deconvolver from a dirty image and
@@ -126,7 +87,7 @@ namespace askap {
                 /// version for compatibility
                 /// @param[in] dirty Dirty image (array)
                 /// @param[in] psf Point Spread Function (array)
-                DeconvolverMultiTermBasisFunction(casacore::Array<T>& dirty, casacore::Array<T>& psf);
+                DeconvolverMultiTermBasisFunction(Array<T>& dirty, Array<T>& psf);
 
                 virtual ~DeconvolverMultiTermBasisFunction();
 
@@ -134,56 +95,18 @@ namespace askap {
                 /// @details The algorithm can work with different basis functions
                 /// PointBasisFunction, MultiScaleBasisFunction.
                 /// @param[in] bf Shared pointer to basisfunction instance
-                void setBasisFunction(boost::shared_ptr<BasisFunction<T> > bf);
+                void setBasisFunction(boost::shared_ptr<BasisFunction<T>> bf);
 
                 /// @brief Return the basis function to be used
                 /// @details The algorithm can work with different basis functions
                 /// PointBasisFunction, MultiScaleBasisFunction
-                boost::shared_ptr<BasisFunction<T> > basisFunction();
+                boost::shared_ptr<BasisFunction<T>> basisFunction();
 
                 /// @brief Set the type of solution used in finding the optimum component
-                void setSolutionType(casacore::String solutionType);
-
-                /// @brief Get the type of solution used in finding the optimum component
-                const casacore::String solutionType();
-
-                /// @brief Set whether to use decoupled residuals
-                void setDecoupled(casacore::Bool decoupled);
-
-                /// @brief Get whether to use decoupled residuals
-                const casacore::Bool decoupled();
-
-                /// @brief Set whether to use a bitmask for the scalemask
-                void setUseScaleBitMask(casacore::Bool useScaleMask);
-
-                /// @brief Set whether to use a pixel list for the scalemask
-                void setUseScalePixels(Bool useScalePixels);
-
-                /// @brief Set whether to use a pixel list for the clean
-                void setUsePixelLists(Bool usePixelLists);
-
-                /// @brief Set pixel list tolerance
-                /// @details Multiply the clean threshold by (1-tolerance) to decide which
-                /// pixels go in the pixel list for a major cycle. Suggested value 0.1
-                void setPixelListTolerance(float tolerance) {
-                    itsPixelListTolerance = tolerance;
-                }
-
-                /// @brief Set pixel list n sigma limit
-                /// @detail Don't put pixels in the pixellist with amplitude < limit*noise
-                /// The noise is determined separately for each scale / residual basis
-                void setPixelListNSigma(float limit) {
-                    itsPixelListNSigma = limit;
-                }
-
-                /// @brief Set the deep cleaning switch for component finding
-                void setDeepCleanMode(casacore::Bool deep);
+                void setSolutionType(const std::string& solutionType);
 
                 // Perform many iterations using OpenMP
                 void ManyIterations();
-
-                /// @brief Get the deep cleaning switch for component finding
-                const casacore::Bool deepCleanMode();
 
                 /// @brief Perform the deconvolution
                 /// @detail This is the main deconvolution method.
@@ -206,20 +129,17 @@ namespace askap {
                 /// @detail Update an existing deconvolver for a changed dirty image
                 /// @param[in] dirty Dirty image (array)
                 /// @param[in] term term to update
-                virtual void updateDirty(casacore::Array<T>& dirty, casacore::uInt term = 0);
+                virtual void updateDirty(Array<T>& dirty, uInt term = 0);
 
                 /// @brief Update only the dirty images
                 /// @detail Update an existing deconvolver for a changed dirty images.
                 /// @param[in] dirty Dirty image (vector of arrays)
-                virtual void updateDirty(casacore::Vector<casacore::Array<T> >& dirty);
-
-	            void setInverseCouplingMatrix(casa::Matrix<casa::Double> &InverseMatrix);
-	            casa::Matrix<casa::Double> getInverseCouplingMatrix();
+                virtual void updateDirty(Vector<Array<T>>& dirty);
 
                 /// @brief export the scale mask
                 /// @detail Give access to the scale mask used in the deconvolution
                 /// @return Matrix<T> scale mask (bitmask of scales for each pixel)
-                const casacore::Matrix<T> scaleMask();
+                const Matrix<T> scaleMask();
 
                 /// @brief import initial scale mask
                 /// @detail Load an initial scale mask to use in the deconvolution
@@ -231,99 +151,100 @@ namespace askap {
                 /// much as possible without losing essential state
                 virtual void releaseMemory();
 
-
             private:
-
-                // Perform one iteration
-                bool oneIteration();
 
                 // Initialise the PSFs - only need to do this once per change in basis functions
                 void initialisePSF();
 
+                // Initialise the residual bases if required
                 void initialiseResidual();
 
+                // Initialise the deep clean masks if required
                 void initialiseMask();
 
                 // Initialise the PSFs - only need to do this once per change in basis functions
                 virtual void initialiseForBasisFunction(bool force);
 
-                void chooseComponent(uInt& optimumBase, casacore::IPosition& absPeakPos, T& absPeakVal, Vector<T>& peakValues);
+                // Not currently implemented, extract from ManyIterations and reinstate after timer update
+                void chooseComponent(uInt& optimumBase, IPosition& absPeakPos, T& absPeakVal, Vector<T>& peakValues);
 
-                void getCoupledResidual(T& absPeakRes);
+                /// Fill the vector of high (or active) pixels for each base
+                void fillHighPixelList(std::vector<std::vector<uInt>>& highPixels, const Matrix<T>& weight);
 
-                #ifdef USE_OPENACC
+                /// Work out the slice of the residual and psf to use
+                void getResidualAndPSFSlice(const IPosition& absPeakPos,
+                    IPosition& shape, IPosition& residualStart, IPosition& psfStart);
 
-                ACCManager<T> itsACCManager;
+                /// Add the optimum basisfunction component to the model
+                void addComponentToModel(const Vector<T>& peakValues,
+                        const IPosition& shape, const IPosition& resStart, const IPosition& psfStart,
+                        const uInt optimumBase, Matrix<T>& model);
 
-                #endif // USE_OPENACC
+                /// Subtract the basisfunction component from all residual bases and terms
+                void subtractPSF(const Vector<T>& peakValues,
+                    const IPosition& shape, const IPosition& resStart, const IPosition& psfStart,
+                    uInt optimumBase);
+
+                /// Subtract the basisfunction component from all residual bases and terms
+                /// This version uses the list of active pixels instead of the full arrays
+                void subtractPSFPixels(const Vector<T>& peakValues, const IPosition& peakPos,
+                    uInt optimumBase, bool useHighPixels, const std::vector<std::vector<uInt>>& highPixels);
 
                 /// Long vector of PSFs
-                casacore::Vector<casacore::Array<T> > itsPsfLongVec;
+                Vector<Matrix<T>> itsPsfLongVec;
 
                 /// Residual images convolved with basis functions, [nx,ny][nterms][nbases]
-                casacore::Vector<casacore::Vector<casacore::Array<T> > > itsResidualBasis;
-
-                /// Residual images for the GPU
-                std::vector<std::vector<T *> > GPUResidualBasis;
-
-                /// Mask images giving the location of all components per basis
-                casacore::Vector<casacore::Array<T> > itsMask;
-
-                /// BitMask image giving the location of all components per base
-                casacore::Matrix<uint> itsScaleMask;
+                Vector<Vector<Matrix<T>>> itsResidualBasis;
 
                 /// Number of scales is limited to 24 as we write the scale mask out as a float image
                 /// we could use std::numeric_limits<T>::digits, but for double
-                /// the uint used doesn't have enough range - anyway 24 scales seems plenty
-                const uint itsMaxScales = 24;
+                /// the uInt used doesn't have enough range - anyway 24 scales seems plenty
+                const uInt itsMaxScales = 24;
 
-                /// Alternative bitmask listing active pixels for each scale
-                /// Need to use ulong/size_t instead of uint for images > 64k^2
-                std::vector<std::set<uint>> itsScalePixels;
+                /// Bitmask listing active pixels for each scale
+                /// Would need to use ulong/size_t instead of uInt for images > 64k^2
+                std::vector<std::set<uInt>> itsScalePixels;
 
                 /// Point spread functions convolved with cross terms
                 // [nxsub,nysub][nterms,nterms][nbases,nbases]
-                casacore::Matrix<casacore::Matrix<casacore::Array<T> > > itsPSFCrossTerms;
+                Matrix<Matrix<Matrix<T>>> itsPSFCrossTerms;
 
                 /// The coupling between different terms for each basis [nterms,nterms][nbases]
-                casacore::Vector<casacore::Matrix<casacore::Double> > itsCouplingMatrix;
+                Vector<Matrix<double>> itsCouplingMatrix;
 
                 /// Inverse of the coupling matrix [nterms,nterms][nbases]
-                casacore::Vector<casacore::Matrix<casacore::Double> > itsInverseCouplingMatrix;
-
-                /// Determinants of the coupling Matrix for each base [nbases]
-                casacore::Vector<casacore::Double> itsDetCouplingMatrix;
+                Vector<Matrix<double>> itsInverseCouplingMatrix;
 
                 /// Basis function used in the deconvolution
-                boost::shared_ptr<BasisFunction<T> > itsBasisFunction;
+                boost::shared_ptr<BasisFunction<T>> itsBasisFunction;
 
                 /// The flux subtracted on each term and scale [nterms][nbases]
-                casa::Vector< casa::Vector<T> > itsTermBaseFlux;
+                Vector< Vector<T>> itsTermBaseFlux;
 
-                casa::Bool itsDirtyChanged;
+                /// Flag to indicate the dirty / residual images have been updated
+                bool itsDirtyChanged;
 
-                casa::Bool itsBasisFunctionChanged;
+                /// FLag to indicate the BasisFunction has been updated
+                bool itsBasisFunctionChanged;
 
-                casa::String itsSolutionType;
+                /// The Clean solution type - MAXBASE or MAXCHISQ
+                std::string itsSolutionType;
 
-                casa::Bool itsDecoupled;
+                /// Use a list of high / active pixels
+                bool itsUsePixelLists;
 
-                casa::Bool itsDeep;
-
-                casa::Bool itsUseScaleMask;
-
-                casa::Bool itsUseScalePixels;
-
-                casa::Bool itsUsePixelLists;
-
+                /// @brief pixel list tolerance
+                /// @details Multiply the clean threshold by (1-tolerance) to decide which
+                /// pixels go in the pixel list for a major cycle. Suggested value 0.1
                 float itsPixelListTolerance;
 
+                /// @brief pixel list n sigma limit
+                /// @detail Don't put pixels in the pixellist with amplitude < limit*noise
+                /// The noise is determined separately for each scale / residual basis
                 float itsPixelListNSigma;
 
-      /// @brief Store the MFS inverse coupling matrix
-      /// @details needed by the restore solver, but it doesn't have all 2N-1 PSFs needed for generation. So store.
-      static casa::Matrix<casa::Double> itsInverseCouplingMatrixCache;
-
+                /// Read a pre-existing scale mask with the name given if not empty
+                std::string itsScaleMaskName;
         };
 
     } // namespace synthesis
