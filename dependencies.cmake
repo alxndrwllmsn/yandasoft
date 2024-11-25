fb0ceb32a6cd9119d46d22cb4e4d186462f4ece2
Minh Vuong <Minh.Vuong@csiro.au>
2024-11-26 10:25:34 +1100

    AXA-2936. Added CXX11 build as a fallback option and Added commit id for the lofar-common and base-askap changes in the dependencies.cmake file to get this package to build

--- dependencies.cmake
+++ dependencies.cmake
@@ -11,9 +11,9 @@
 #     Identifier                   tag/branch       cache  type      description                        force it
 set ( ASKAP_CMAKE_TAG              tags/1.5.0      CACHE  STRING    "askap.cmake tools"                FORCE )
 set ( ASKAP_DEV_TAG                tags/2.13.0     CACHE  STRING    "askap dev tools"                  FORCE )
-set ( LOFAR_COMMON_TAG             tags/1.2.1      CACHE  STRING    "lofar-common version"             FORCE )
+set ( LOFAR_COMMON_TAG             f35117f80d12c9bd4a363c23fb00c8e0c49b1cee      CACHE  STRING    "lofar-common version"             FORCE )
 set ( LOFAR_BLOB_TAG               tags/1.3.1      CACHE  STRING    "lofar-blob version"               FORCE )
-set ( BASE_ASKAP_TAG               tags/1.7.2      CACHE  STRING    "base-askap version"               FORCE )
+set ( BASE_ASKAP_TAG               ffd9370a6e5ebc5e3f07e2383bca2df431dd8269      CACHE  STRING    "base-askap version"               FORCE )
 set ( BASE_LOGFILTERS_TAG          tags/1.4.1      CACHE  STRING    "base-logfilters version"          FORCE )
 set ( BASE_IMAGEMATH_TAG           tags/1.14.3     CACHE  STRING    "base-imagemath version"           FORCE )
 set ( BASE_ASKAPPARALLEL_TAG       tags/1.9.3      CACHE  STRING    "base-askapparallel version"       FORCE )
