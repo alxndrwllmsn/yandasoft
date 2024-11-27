a2a9895492273840f0d69964e35e2c761e6b636b
Minh Vuong <minh.vuong@csiro.au>
2024-11-27 08:54:59 +0800

    AXA-2936. Updated dependencies.cmake file with merge commit id for base-accessors and base-components instead of feature brand commit ids

--- dependencies.cmake
+++ dependencies.cmake
@@ -17,6 +17,6 @@ set ( BASE_ASKAP_TAG               ffd9370a6e5ebc5e3f07e2383bca2df431dd8269    C
 set ( BASE_IMAGEMATH_TAG           9ce81c9021c013eb1dbdce83fe6e94a6228d68b9    CACHE  STRING    "base-imagemath version"           FORCE )
 set ( BASE_ASKAPPARALLEL_TAG       1ccc9663cf58a71007db5dc48e4ad3beb0a4d419    CACHE  STRING    "base-askapparallel version"       FORCE )
 set ( BASE_SCIMATH_TAG             feb949247c760972ae3c7a0936bb7869ee3ebc35    CACHE  STRING    "base-scimath version"             FORCE )
-set ( BASE_ACCESSORS_TAG           9adc8a1382bd6c1a0df2d34e2f0949f901fe0aec    CACHE  STRING    "base-accessors version"           FORCE )
-set ( BASE_COMPONENTS_TAG          b15b15c8e21790e48baffdbeedaf030143fd465d    CACHE  STRING    "base-components version"          FORCE )
+set ( BASE_ACCESSORS_TAG           016599d64669fd8ad5b71d272b2b2f07f7ccb6d6    CACHE  STRING    "base-accessors version"           FORCE )
+set ( BASE_COMPONENTS_TAG          96024d1b9cdce2e6e29685c09c60c0c28f67d5b5    CACHE  STRING    "base-components version"          FORCE )
 # TOS related repos are not versioned yet, so pinned this build with commit hash values.
