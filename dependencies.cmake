8508c8b1a266aaef4994b7e48bf4806507b55875
Minh Vuong <minh.vuong@csiro.au>
2025-01-24 09:14:22 +0800

    AXA-1634. modified .gitlab-ci.yml file to add another build stage for ENABLE_FLOAT_IMAGE_PARAMS=NO

--- dependencies.cmake
+++ dependencies.cmake
@@ -17,6 +17,7 @@ set ( BASE_ASKAP_TAG               044f3ef17c4209e39d45a32763c3edc22563dbb4    C
 set ( BASE_LOGFILTERS_TAG          ca5f91a431a0df668199a9c6f05383bc0c38e135    CACHE  STRING    "base-logfilters version"          FORCE )
 set ( BASE_IMAGEMATH_TAG           df34e16364d5cb77334ae25cfcf21ecf93e8dac4    CACHE  STRING    "base-imagemath version"           FORCE )
 set ( BASE_ASKAPPARALLEL_TAG       7a08893da050e4c710984eaa93cc1f9208eca5c2    CACHE  STRING    "base-askapparallel version"       FORCE )
+#set ( BASE_SCIMATH_TAG             12e2cd852e58d23740dd8b06b472ed6d82a3ddb6    CACHE  STRING    "base-scimath version"             FORCE )
 set ( BASE_SCIMATH_TAG             12e2cd852e58d23740dd8b06b472ed6d82a3ddb6    CACHE  STRING    "base-scimath version"             FORCE )
 set ( BASE_ACCESSORS_TAG           016599d64669fd8ad5b71d272b2b2f07f7ccb6d6    CACHE  STRING    "base-accessors version"           FORCE )
 set ( BASE_COMPONENTS_TAG          96024d1b9cdce2e6e29685c09c60c0c28f67d5b5    CACHE  STRING    "base-components version"          FORCE )
