40549c6bfa9278c9857adcf541b5d10a509aa34b
Minh Vuong <minh.vuong@csiro.au>
2025-01-24 09:14:22 +0800

    AXA-1634. modified .gitlab-ci.yml file to add another build stage for ENABLE_FLOAT_IMAGE_PARAMS=NO

--- dependencies.cmake
+++ dependencies.cmake
@@ -13,10 +13,12 @@ set ( ASKAP_CMAKE_TAG              tags/1.6.0      CACHE  STRING    "askap.cmake
 set ( ASKAP_DEV_TAG                tags/2.13.0     CACHE  STRING    "askap dev tools"                  FORCE )
 set ( LOFAR_COMMON_TAG             tags/1.3.0      CACHE  STRING    "lofar-common version"             FORCE )
 set ( LOFAR_BLOB_TAG               tags/1.3.1      CACHE  STRING    "lofar-blob version"               FORCE )
+
 set ( BASE_ASKAP_TAG               tags/1.8.0      CACHE  STRING    "base-askap version"               FORCE )
 set ( BASE_LOGFILTERS_TAG          tags/1.5.0      CACHE  STRING    "base-logfilters version"          FORCE )
 set ( BASE_IMAGEMATH_TAG           tags/1.15.0     CACHE  STRING    "base-imagemath version"           FORCE )
 set ( BASE_ASKAPPARALLEL_TAG       tags/1.10.0     CACHE  STRING    "base-askapparallel version"       FORCE )
+set ( BASE_SCIMATH_TAG             12e2cd852e58d23740dd8b06b472ed6d82a3ddb6    CACHE  STRING    "base-scimath version"             FORCE )
 set ( BASE_SCIMATH_TAG             tags/1.15.0     CACHE  STRING    "base-scimath version"             FORCE )
 set ( BASE_ACCESSORS_TAG           tags/1.17.0     CACHE  STRING    "base-accessors version"           FORCE )
 set ( BASE_COMPONENTS_TAG          tags/1.14.0     CACHE  STRING    "base-components version"          FORCE )
